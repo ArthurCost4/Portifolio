04fbf67f23aa9457efb1abacffa3e258758d7945
Arthur Costa <arthurlinhares69@gmail.com>
2022-05-15 19:27:55 -0300

    add slider dinamico
    Feita a função que faz o slider passa a foto sozinho

--- public/js/fixo.js
+++ public/js/fixo.js
@@ -1,4 +1,13 @@
 const responsiveBtn = document.getElementById('responsiveBtn')
+var count = 1
+
+setInterval(()=>{
+    document.getElementById('radio' + count).checked = true;
+    count++
+    (count > 5) ? count = 1 : count
+}, 5000)
+
+
 
 function abrirMenu(){
     const menuHeader = document.getElementById('menuHeader')
